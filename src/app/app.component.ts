2b37efa4d3b11d6148e124e056b1f6611946ae7b
AshyyMc <60858363+AshyyMc@users.noreply.github.com>
2021-02-19 17:47:33 +0000

    This commit is to add more articles to appear and articles to be added like we have added to the form.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
-
+import {Article} from "./article/article.model";
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  articles:Article[];
+
+  constructor() {
+    this.articles=[
+      new Article("Angular", "http://angular.io",5),
+      new Article("Google", "http://Google.io",3),
+      new Article("TikTok", "http://TikTok.io",1)
+    ];
+  }
  addArticle(title:HTMLInputElement, link:HTMLInputElement):Boolean {
+   this.articles.push(new Article(title.value, link.value));
+   title.value ='';
+   link.value = '';
+
    console.log('Adding Article Title $(title.value): and adding link $(title.value) :');
   return false;
  }
--- src/app/article/article.component.ts
+++ src/app/article/article.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { Article } from './article.model';
 
 @Component({
@@ -8,10 +8,10 @@ import { Article } from './article.model';
 })
 export class ArticleComponent implements OnInit {
 
-article:Article;
+@Input() article:Article;
 
   constructor() {
-    this.article=new Article("Angular","http://angular.io")
+    //this.article=new Article("Angular","http://angular.io")
     
    }
 
