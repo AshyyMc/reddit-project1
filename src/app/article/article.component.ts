1c58562af34fd61db26ffef0776fe6fd33850324
AshyyMc <60858363+AshyyMc@users.noreply.github.com>
2021-02-19 17:28:15 +0000

    I created a new file in article. I got article.component.html for referencng the article class.  I have article.component.ts where it automactically imported the article from the article model.  I then updated the constructor and simplified the voteup/votedown methods. In artical.model.ts total stand alone model for data.

--- src/app/article/article.component.ts
+++ src/app/article/article.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Article } from './article.model';
 
 @Component({
   selector: 'app-article',
@@ -7,26 +8,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ArticleComponent implements OnInit {
 
-  votes:number;
-  title:string;
-  link:string;
+article:Article;
 
   constructor() {
-    this.title="Angular";
-    this.title="https://angular.io";
-    this.votes=10;
+    this.article=new Article("Angular","http://angular.io")
+    
    }
 
   ngOnInit()  {
   }
 
   voteUp():boolean {
-this.votes ++;
+this.article.voteUp();
 return false;
   }
 
   voteDown():boolean {
-    this.votes --;
+    this.article.voteDown();
     return false;
   }
 }
